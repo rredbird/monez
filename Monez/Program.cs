e9668c4da9397ce550dee9720f966ab4c579a33a
Robin Ruth <robin.ruth.tub@gmail.com>
2018-02-17 15:01:18 +0100

    Projekt erstellt NServiceBus Setup angefangen

---
+++ Monez/Program.cs
@@ -0,0 +1,27 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Monez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public Program()
+        {
+
+        }
+
+        public async Task StartNServiceBus()
+        {
+            var endpointConfiguration = new EndpointConfiguration("ConsoleUI");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+
+        } 
+    }
+}
